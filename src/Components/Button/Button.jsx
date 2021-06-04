359fef3762782bfe1a8b8c435b469878c32f8de7
Jose Luna <lunaxp5@gmail.com>
2021-06-03 23:19:59 -0400

    definition cursor, text, creation buttons

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -5,6 +5,7 @@ const Button = (props) => {
   return (
     <>
       {props?.as ? (
+        props?.as==='label'?
         <label
           className={`${props.color ? `btn-${props.color}` : `btn-primary`}  ${
             props?.full ? `full` : ``
@@ -15,14 +16,20 @@ const Button = (props) => {
             {props?.icon ? <i>{props.icon}</i> : null}
             <p>{props.children}</p>
           </div>
-        </label>
-      ) : (
+        </label>:
         <a className={props.color ? `btn-${props.color}` : `btn-primary`} {...props}>
           <div className="btn-content">
             {props?.icon ? <i>{props.icon}</i> : null}
             <p>{props.children}</p>
           </div>
         </a>
+      ) : (
+        <button className={props.color ? `btn-${props.color}` : `btn-primary`} {...props}>
+          <div className="btn-content">
+            {props?.icon ? <i>{props.icon}</i> : null}
+            <p>{props.children}</p>
+          </div>
+        </button>
       )}
     </>
   );
