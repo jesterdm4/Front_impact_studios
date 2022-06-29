aed0db4ced275a6b3b92c5da38bc5a9a6496f3fe
Jose Luna <lunaxp5@gmail.com>
2022-06-29 09:15:06 -0400

    fix styles

--- src/Components/Frame2/Frame2.js
+++ src/Components/Frame2/Frame2.js
@@ -1,20 +1,22 @@
 import "./Frame2.scss";
-
 const Frame2 = () => {
-    return (
-        <>
-            <div className="total-frame2-container">
-                <div className="frame2-container">
-                    <h1 className="text1-frame2">
-                        experiencias de
-                    </h1>
-                    <h1 className="text2-frame2">
-                        _impacto
-                    </h1>
-                </div>
-            </div>
-        </>
-    );
-}
+  return (
+    <>
+      <div className="total-frame2-container">
+        <div className="first-container">
+          <div className="first-div">
+            <p>Construimos</p>
+          </div>
+        </div>
+
+        {/* convertir en un coponente donde puedas user children y una prop para colores, ver componente Text*/}
+        <div className="frame2-container">
+          <h1 className="text-frame2">experiencias de</h1>
+          <h1 className="text-frame2">_impacto</h1>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Frame2;
--- src/View/Home/Home.js
+++ src/View/Home/Home.js
@@ -1,28 +1,17 @@
 import React from "react";
 import "./home.scss";
 
-
 import { Frame2, Header, Text } from "../../Components";
 
 const Home = (props) => {
   return (
     <>
       <div className="black-container">
-
         <Header />
-        <div className="first-container">
-          <div className="first-div">
-            <p>Construimos</p>
-          </div>
-        </div>
 
         <Frame2 />
-
-
-
       </div>
     </>
-  
   );
 };
 export default Home;
