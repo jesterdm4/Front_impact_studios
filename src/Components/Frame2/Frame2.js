0fc779712f883cc4727f9324e446859fd73b7ac5
Jose <lunaxp5@gmail.com>
2022-06-30 05:43:33 -0400

    coments in frame2.js - problems with TextBox

--- src/Components/Frame2/Frame2.js
+++ src/Components/Frame2/Frame2.js
@@ -36,13 +36,18 @@ const Frame2 = () => {
 
         {/*
         
-        comentario previo a la introduccion de la segunda prop
+        comentario previo a la introduccion de la segunda prop en el componente TextBox
 
-          por otro lado, el componente textBox tiene un limite de 18 caracteres
+          por otro lado, el componente textBox tiene un limite de 18 caracteres,
           independientemente de lo escrito en el ni hara crecer el div que lo
           contiene ni tampoco se extendera mas alla de el, creara un salto de 
           linea automaticamente, este mismo salto ocurre cuando se usa un espacio
           en al gun punto de la propiedad text 
+
+
+
+
+
 
         comentario despues de la introduccion de la segunda prop
 
@@ -51,7 +56,14 @@ const Frame2 = () => {
             
             sera por esto que se separo la palabra con un piso??
 
-              esto genera el problema de que no se como darle un estilo a solo una propiedad de un componente =/ 
+                es posible darle un estilo especifico a cada prop del componente?
+
+          
+          al final se logro posicionar el texto correctamente gracias a tener 2 props 
+          y que se eliminara el comando   transform: rotate(-2deg);   de las letras para
+          solo dejarse en el contenedor, esto mueve no solo el contenedor si no todo 
+          lo que tiene dentro, el detalle con esto es que nunca se soluciono el problema
+          principal de que se genera el salto de linea
 
         */}
 
@@ -61,12 +73,14 @@ const Frame2 = () => {
         />
 
         {/*
-          ambos scss, tanto del frame2-container y su texto como el scss del componente TextBox son identicos, osea que el problema reside en el componente
+          ambos scss, tanto del frame2-container y su texto como el scss del componente 
+          TextBox son identicos (exepto por la linea   transform: rotate(-2deg);  en su texto),
+          osea que el problema reside en el componente
 
-          sera que una propiedad de texto tiene limites de caracteres? ---> pues no C=
+          sera que una propiedad de texto tiene limites de caracteres? ---> pues al parecer no C=
 
-            en caso de ser de ayuda este problema tambien ocurre en first-container, por suerte al ser solo una palabra los saltos de linea por barra de espacio no resultan 
-            un problema
+            en caso de ser de ayuda este problema tambien ocurre en first-container, por suerte al ser solo una palabra los saltos de linea por barra 
+            de espacio no resultan un problema
         */}
 
       </div>
@@ -75,5 +89,7 @@ const Frame2 = () => {
     </>
   );
 };
+
+
 
 export default Frame2;
