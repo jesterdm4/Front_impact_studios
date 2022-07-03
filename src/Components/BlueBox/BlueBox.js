2aad552bc3210f271b30c705646a7f0810c11f04
Jose <lunaxp5@gmail.com>
2022-07-03 13:34:51 -0400

    BlueBoex ready but can be improved

--- src/Components/BlueBox/BlueBox.js
+++ src/Components/BlueBox/BlueBox.js
@@ -7,15 +7,15 @@ const BlueBox = () => {
       <>
         <div className='BlueBox1'>
 
-            <div className='BlueBox2'>2</div>
-            <div className='BlueBox3'>3</div>
+            <div className='BlueBox2'></div>
+            <div className='BlueBox3'></div>
 
             <div className='TextoBlanco'>
                 Especialistas en el Desarrollado y Diseño de Software, para procesos de Transformación Digital y construcción de tecnologias emergentes
             </div>
 
-            <div className='BlueBox4'>4</div>
-            <div className='BlueBox5'>5</div>
+            <div className='BlueBox4'></div>
+            <div className='BlueBox5'></div>
         </div>
       </>
     );
